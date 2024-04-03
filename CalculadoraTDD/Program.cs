e71fd61a7595f9f5bf0eb5adbe0261f600334a64
PkMs7 <patrickmarques2009@hotmail.com>
2024-04-03 14:17:19 -0300

    Validação das impressões dos dados do programa em execução concluindo a entrega da primeira versão do projeto

--- CalculadoraTDD/Program.cs
+++ CalculadoraTDD/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello World!");
+﻿using CalculadoraTDD;
+
+Calculadora calc = new Calculadora();
+
+Console.WriteLine(calc.Somar(2, 2));
+Console.WriteLine(calc.Subtrair(2, 2));
+Console.WriteLine(calc.Dividir(2, 2));
+Console.WriteLine(calc.Multiplicar(2, 2));
+
+foreach(var elemento in calc.GerarHistorico())
+{
+    Console.WriteLine(elemento);
+}
