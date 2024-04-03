9be6593232b0293b02c1912865a7ceb475266dcd
PkMs7 <patrickmarques2009@hotmail.com>
2024-04-03 14:09:25 -0300

    Implementação do método gerarHistorico, passando então no teste criado anteriormente

--- CalculadoraTDD/Calculadora.cs
+++ CalculadoraTDD/Calculadora.cs
@@ -2,29 +2,45 @@ namespace CalculadoraTDD
 {
     public class Calculadora
     {
+        private List<string> historico;
+
+        public Calculadora()
+        {
+            historico = new List<string>();
+        }
+
         public int Somar(int num1, int num2)
         {
-            return num1 + num2;
+            int resultado = num1 + num2;
+            historico.Insert(0, $"Resultado de {num1} + {num2} = {resultado}");
+            return resultado;
         }
 
         public int Subtrair(int num1, int num2)
         {
-            return num1 - num2;
+            int resultado = num1 - num2;
+            historico.Insert(0, $"Resultado de {num1} - {num2} = {resultado}");
+            return resultado;
         }
 
         public int Dividir(int num1, int num2)
         {
-            return num1 / num2;
+            int resultado = num1 / num2;
+            historico.Insert(0, $"Resultado de {num1} / {num2} = {resultado}");
+            return resultado;
         }
 
         public int Multiplicar(int num1, int num2)
         {
-            return num1 * num2;
+            int resultado = num1 * num2;
+            historico.Insert(0, $"Resultado de {num1} * {num2} = {resultado}");
+            return resultado;
         }
 
         public List<string> GerarHistorico()
         {
-            return new List<string>();
+            historico.RemoveRange(3, historico.Count - 3);
+            return historico;
         }
     }
 }
